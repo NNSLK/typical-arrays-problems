a4f5229c71613294d92fe4886351061584d46f96
NNSLK <gogamemann@gmail.com>
2020-10-01 23:03:30 +0300

    init: start array task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length) {
+        return Math.min.apply(Math, array);
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length) {
+        return Math.max.apply(Math, array);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length) {
+        let sum = 0;
+        let result = 0;
+        for (let i = 0; i < array.length; i++) {
+            if (array.length > 0) {
+                sum += array[i];
+                result = sum / array.length;
+            }
+        }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+        return result;
+    } else {
+        return 0;
+    }
+};
